6e388fe0d02c8ea511e1448cb36520ff4f4504c7
nadaka <yusuke.nadaka@gmail.com>
2022-08-21 17:28:22 +0900

    doc: YARD

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,4 +1,8 @@
 class Parser
+  # TODO: もうちょい良い実装がありそう
+  # 引数の文字列の先頭の整数をパースし、結果と残りの文字列を返す
+  # @param [String] s 文字列
+  # @return [nil, Array<String>]
   def digits(s)
     target = []
     other = []
